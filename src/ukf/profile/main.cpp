00c4d2ab3c6fb6171c718d710a878ae2a9944ae5
Paul D'Angio <pcdangio@gmail.com>
2021-02-19 16:40:26 -0500

    ukf/ukf_a: initial commit of profile

---
+++ src/ukf/profile/main.cpp
@@ -0,0 +1,54 @@
+#include <kalman_filter/ukf.hpp>
+
+#include <chrono>
+#include <iostream>
+#include <iomanip>
+
+class model_t
+    : public kalman_filter::ukf_t
+{
+public:
+    model_t()
+        :ukf_t(30,30)
+    {
+
+    }
+
+private:
+    void state_transition(const Eigen::VectorXd& xp, Eigen::VectorXd& x) const override
+    {
+        x = xp;
+    }
+    void observation(const Eigen::VectorXd& x, Eigen::VectorXd& z) const override
+    {
+        z = x;
+    }
+};
+
+int32_t main(int32_t argc, char** argv)
+{
+    uint32_t n_runs = 10000;
+
+    model_t ukf;
+
+    // Start timing.
+    auto start_time = std::chrono::high_resolution_clock::now();
+    // Start loop.
+    for(uint32_t n = 0; n < n_runs; ++n)
+    {
+        // Add observations.
+        for(uint32_t z = 0; z < ukf.n_observers(); ++z)
+        {
+            ukf.new_observation(z, 10.0);
+        }
+
+        // Iterate.
+        ukf.iterate();
+    }
+    // Stop timing.
+    auto stop_time = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<double_t> delta = stop_time - start_time;
+
+    std::cout << std::fixed << std::setprecision(10) << delta.count() / static_cast<double_t>(n_runs) << std::endl;
+}
